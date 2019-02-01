f771b35d89831b3d5bf00ac6726168790e87699d
iloughman <iloughman@gmail.com>
2019-01-31 20:05:44 -0500

    stopping point with hardcoded data before moving to prisma api client

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  },
+  {
+    id: 'link-2',
+    url: 'www.something.com',
+    description: 'Is this worth it'
+  }
+]
+
+let idCount = links.length
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+    link: (parent, args) => {
+      return links.find(el => el.id === args.id)
+    }
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      }
+      links.push(link)
+      return link
+    },
+    updateLink: (parent, {id, url, description}) => {
+      let link = links.find(el => el.id === id)
+      link.url = url
+      link.description = description
+      return link;
+    }
+  }
+  // Link: {
+  //   id: (parent) => parent.id,
+  //   description: (parent) => parent.description,
+  //   url: (parent) => parent.url
+  // }
+}
+
+// 3
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
